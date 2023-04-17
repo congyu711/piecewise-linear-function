813596fd08750ad724ebc5c1d821d33b963bdd0a
congyu711 <sxlxcsxlxc@gmail.com>
2023-04-17 14:39:01 +0800

    _max(f,g)

--- piecewise-linear.cc
+++ piecewise-linear.cc
@@ -4,6 +4,35 @@ using namespace plf;
 using namespace std;
 const double eps=1e-9;
 
+double plf::_intersection(const line_segment& f, const line_segment& g)
+{
+  if(f.a==g.a)  return std::numeric_limits<double>::max();
+  return (g.b-f.b)/(f.a-g.a);
+}
+
+void linesegment2breakpoints(const piecewise_linear_func &g, double l, double r, 
+                             vector<double>& bkpts, vector<vector<line_segment>::const_iterator>& ptrs)
+{
+  bkpts.clear();ptrs.clear();
+  bkpts.push_back(l);
+  for(auto it=g.lines.cbegin();it!=g.lines.cend();it++)
+  {
+    auto e=*it;
+    if(e.l!=bkpts.back()) // there is an undefined interval
+    {
+      bkpts.push_back(e.l);
+      ptrs.push_back(g.lines.cend());
+    }
+    bkpts.push_back(e.r);
+    ptrs.push_back(it);
+  }
+  if(bkpts.back()!=r)
+  {
+    bkpts.push_back(r);
+    ptrs.push_back(g.lines.cend());
+  }
+}
+
 piecewise_linear_func piecewise_linear_func::operator+(const piecewise_linear_func &g)
 {
   piecewise_linear_func res;
@@ -11,39 +40,9 @@ piecewise_linear_func piecewise_linear_func::operator+(const piecewise_linear_fu
   res.r=std::max(r,g.r);
   vector<double> bkpts,g_bkpts; // breakpoints
   vector<vector<line_segment>::const_iterator> ptrs,g_ptrs; // pointers
-  bkpts.push_back(res.l);g_bkpts.push_back(res.l);
-  for(auto it=lines.cbegin();it!=lines.cend();it++)
-  {
-    auto e=*it;
-    if(e.l!=bkpts.back()) // there is an undefined interval
-    {
-      bkpts.push_back(e.l);
-      ptrs.push_back(lines.cend());
-    }
-    bkpts.push_back(e.r);
-    ptrs.push_back(it);
-  }
-  if(bkpts.back()!=res.r)
-  {
-    bkpts.push_back(res.r);
-    ptrs.push_back(lines.cend());
-  }
-  for(auto it=g.lines.cbegin();it!=g.lines.cend();it++)
-  {
-    auto e=*it;
-    if(e.l!=g_bkpts.back()) // there is an undefined interval
-    {
-      g_bkpts.push_back(e.l);
-      g_ptrs.push_back(g.lines.cend());
-    }
-    g_bkpts.push_back(e.r);
-    g_ptrs.push_back(it);
-  }
-  if(g_bkpts.back()!=res.r)
-  {
-    g_bkpts.push_back(res.r);
-    g_ptrs.push_back(g.lines.cend());
-  }
+  linesegment2breakpoints(ref(*this),res.l,res.r,bkpts,ptrs);
+  linesegment2breakpoints(g,res.l,res.r,g_bkpts,g_ptrs);
+
   double left=res.l;
   for(int i=1,j=1;i<bkpts.size()&&j<g_bkpts.size();)
   {
@@ -70,5 +69,56 @@ piecewise_linear_func piecewise_linear_func::operator+(const piecewise_linear_fu
     res.lines.push_back(tmp);
   }
 
+  return res;
+}
+
+piecewise_linear_func plf::_max(const piecewise_linear_func& f, const piecewise_linear_func& g)
+{
+  piecewise_linear_func res;
+  res.l=std::min(f.l,g.l);
+  res.r=std::max(f.r,g.r);
+  vector<double> f_bkpts,g_bkpts; // breakpoints
+  vector<vector<line_segment>::const_iterator> f_ptrs,g_ptrs; // pointers
+  linesegment2breakpoints(f,res.l,res.r,f_bkpts,f_ptrs);
+  linesegment2breakpoints(g,res.l,res.r,g_bkpts,g_ptrs);
+  double left=res.l;
+  for(int i=1,j=1;i<f_bkpts.size()&&j<g_bkpts.size();)
+  {
+    line_segment tmp,_f,_g;
+    tmp.l=left;
+    if(f_ptrs[i-1]!=f.lines.cend()) _f=*f_ptrs[i-1];
+    else _f.r=f_bkpts[i];
+    if(g_ptrs[j-1]!=g.lines.cend()) _g=*g_ptrs[j-1];
+    else _g.r=g_bkpts[j];
+    _f.l=_g.l=left;
+    // compute the intersection
+    double cross=_intersection(_f,_g);
+    if(_f.getval(left)<_g.getval(left)) tmp.a=_g.a,tmp.b=_g.b;
+    else tmp.a=_f.a,tmp.b=_f.b;
+    // no valid intersection.
+    if(cross==numeric_limits<double>::max()||cross>min(_f.r,_g.r)||cross<left)
+    {
+      tmp.r=min(f_bkpts[i],g_bkpts[j]);
+      res.lines.push_back(tmp);
+    }
+    else
+    {
+      tmp.r=cross;
+      res.lines.push_back(tmp);
+      // add another line segment
+      double __a=(tmp.a==_f.a?_g.a:_f.a);
+      double __b=(tmp.a==_f.a?_g.b:_f.b);
+      res.lines.push_back(line_segment(cross,min(f_bkpts[i],g_bkpts[j]),__a,__b));
+    }
+
+    if(f_bkpts[i]<g_bkpts[j])
+      i++;
+    else if(f_bkpts[i]>g_bkpts[j])
+      j++;
+    else  // bkpts[i]==g_bkpts[j]
+      i++,j++;
+
+    left=min(f_bkpts[i],g_bkpts[j]);
+  }
   return res;
 }
--- piecewise-linear.hh
+++ piecewise-linear.hh
@@ -1,4 +1,8 @@
+#ifndef _PIECEWISE_LINEAR_
+#define _PIECEWISE_LINEAR_
+
 #include <vector>
+#include <limits>
 // #include <algorithm>
 
 namespace plf // piecewise linear function
@@ -11,7 +15,12 @@ namespace plf // piecewise linear function
     double a, b; // y=ax+b, x \in [l,r]
     line_segment(double _l,double _r,double _a,double _b):l(_l),r(_r),a(_a),b(_b){}
     line_segment():l(0),r(0),a(0),b(0){}
+    double getval(double x)
+    {
+      return a*x+b;
+    }
   };
+  double _intersection(const line_segment& f, const line_segment& g);
 
   class piecewise_linear_func
   {
@@ -21,4 +30,7 @@ namespace plf // piecewise linear function
                                   // y=0 for undefined intervals.
     piecewise_linear_func operator+(const piecewise_linear_func& g);
   };
+  piecewise_linear_func _max(const piecewise_linear_func& f, const piecewise_linear_func& g);
 }
+
+#endif
